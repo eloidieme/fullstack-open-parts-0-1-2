456a851dca98038fa27dcddf481d755846e2146a
Eloi Dieme <eloidieme@gmail.com>
2023-10-10 23:53:24 +0200

    unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
-    <div>Hello</div>
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text = 'good'/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text = 'neutral'/>
+      <Button handleClick={() => setBad(bad + 1)} text = 'bad'/>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {good + neutral + bad}</p>
+      <p>average {(good*1 + bad*(-1))/(good + bad + neutral)}</p>
+      <p>positive {100*good/(good+neutral+bad)} %</p>
+    </div>
   )
 }
 
