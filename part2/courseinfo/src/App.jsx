ec179f9d2b23dc6d4d286c6a2f147b2bb3f4c765
Eloi Dieme <eloidieme@gmail.com>
2023-10-12 17:09:22 +0200

    courseinfo separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,41 +1,4 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises} 
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-      {props.parts.map(part => <Part key = {part.id} part = {part.name} exercises = {part.exercises} />)}
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <b>total of {props.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)} exercises</b>
-  )
-}
-
-const Course = ({ courses }) => {
-  return (courses.map(course => 
-    <div key = {course.id}>
-      <Header course = {course.name}/>
-      <Content parts = {course.parts}/>
-      <Total parts = {course.parts}/>
-    </div>
-    )
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
